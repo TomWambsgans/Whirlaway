4fa38c3140f2e26048e9b4eb1d13643ea4af1ce4
Tom Wambsgans <101908066+TomWambsgans@users.noreply.github.com>
2025-05-28 18:00:05 +0400

    Merge pull request #5 from tcoratger/utils-misc
    utils: simplify misc

--- crates/utils/src/misc.rs
+++ crates/utils/src/misc.rs
@@ -1,12 +1,10 @@
+#[inline(always)]
 pub const fn log2_up(x: usize) -> usize {
-    // rounded up
-    (if x == 0 {
+    if x == 0 {
         0
-    } else if x.is_power_of_two() {
-        1usize.leading_zeros() - x.leading_zeros()
     } else {
-        0usize.leading_zeros() - x.leading_zeros()
-    }) as usize
+        usize::BITS as usize - (x - 1).leading_zeros() as usize
+    }
 }
 
 pub fn count_ending_zero_bits(buff: &[u8]) -> usize {
