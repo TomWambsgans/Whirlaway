79d6f231698b5aa49e0a6c007c699674b91c9664
Thomas Coratger <thomas.coratger@gmail.com>
2025-06-17 12:15:52 +0200

    rm keccak in utils

--- crates/utils/src/keccak.rs
+++
@@ -1,8 +0,0 @@
-use p3_keccak::Keccak256Hash;
-use p3_symmetric::CryptographicHasher;
-#[derive(Debug, Clone, PartialEq, Eq, Default)]
-pub struct KeccakDigest(pub [u8; 32]);
-
-pub fn keccak256(data: &[u8]) -> KeccakDigest {
-    KeccakDigest(Keccak256Hash.hash_slice(data))
-}
