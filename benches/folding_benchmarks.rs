ef4800bdaf4be2b11fa0f1064de6973f25c1d21a
Nicole <nicole.graus@lambdaclass.com>
2025-08-20 12:36:26 -0300

    finish benchmarks

--- benches/folding_benchmarks.rs
+++ benches/folding_benchmarks.rs
@@ -56,9 +56,77 @@ fn create_random_extension_field_element_scalars(log_n: usize) -> Vec<EF> {
         .collect()
 }
 
-// First round (skip == 4 and polys in base field), scalars in base field (small)
+// Represents the case: Round 0 (polynomials in base field), skips == 1, scalars in base field.
+fn bench_fold_multilinear_in_small_field_with_base_field_polys(c: &mut Criterion) {
+    let mut group = c.benchmark_group(
+        "fold_multilinear_in_small_field with base field polys (Round 0) and skips = 1",
+    );
+    group
+        .sample_size(SAMPLE_SIZE)
+        .measurement_time(Duration::from_secs(MEASUREMENT_TIME));
+
+    let log2_rows_list = [22];
+
+    for &log_2_rows in &log2_rows_list {
+        let polys = create_random_base_field_element_polys(log_2_rows);
+        let scalars = create_random_base_field_element_scalars(1);
+
+        group.bench_with_input(
+            BenchmarkId::from_parameter(format!("Rows: 2ˆ{}", log_2_rows)),
+            &polys,
+            |b, polys| {
+                b.iter(|| {
+                    fold_multilinear_in_small_field(
+                        black_box(polys),
+                        black_box(&scalars),
+                        RoundType::WithNoSkips,
+                    )
+                })
+            },
+        );
+    }
+
+    group.finish();
+}
+
+// Represents the case: Rounds > 0 (polynomials in extension field), skips == 1, scalars in base field.
+fn bench_fold_multilinear_in_small_field_with_extension_field_polys(c: &mut Criterion) {
+    let mut group = c.benchmark_group(
+        "fold_multilinear_in_small_field with extension field polys (Rounds > 0) and skips = 1",
+    );
+    group
+        .sample_size(SAMPLE_SIZE)
+        .measurement_time(Duration::from_secs(MEASUREMENT_TIME));
+
+    let log2_rows_list = [22];
+
+    for &log_2_rows in &log2_rows_list {
+        let polys = create_random_extension_field_element_polys(log_2_rows);
+        let scalars = create_random_base_field_element_scalars(1);
+
+        group.bench_with_input(
+            BenchmarkId::from_parameter(format!("Rows: 2ˆ{}", log_2_rows)),
+            &polys,
+            |b, polys| {
+                b.iter(|| {
+                    fold_multilinear_in_small_field(
+                        black_box(polys),
+                        black_box(&scalars),
+                        RoundType::WithNoSkips,
+                    )
+                })
+            },
+        );
+    }
+
+    group.finish();
+}
+
+// Represents the case: Round 0 (polynomials in base field), skips == 4, scalars in base field.
 fn bench_fold_multilinear_in_small_field_with_skip(c: &mut Criterion) {
-    let mut group = c.benchmark_group("fold_multilinear_small_field_with_skip");
+    let mut group = c.benchmark_group(
+        "fold_multilinear_in_small_field with base field polys (Round 0) and skips = 4",
+    );
     group
         .sample_size(SAMPLE_SIZE)
         .measurement_time(Duration::from_secs(MEASUREMENT_TIME));
@@ -88,9 +156,77 @@ fn bench_fold_multilinear_in_small_field_with_skip(c: &mut Criterion) {
     group.finish();
 }
 
-// First round (skip == 4 and polys in base field), scalars in extension field (large)
+// Represents the case: Round 0 (polynomials in base field), skips == 1, scalars in extension field.
+fn bench_fold_multilinear_in_large_field_with_base_field_polys(c: &mut Criterion) {
+    let mut group = c.benchmark_group(
+        "fold_multilinear_in_large_field with base field polys (Round 0) and skips = 1",
+    );
+    group
+        .sample_size(SAMPLE_SIZE)
+        .measurement_time(Duration::from_secs(MEASUREMENT_TIME));
+
+    let log2_rows_list = [22];
+
+    for &log_2_rows in &log2_rows_list {
+        let polys = create_random_base_field_element_polys(log_2_rows);
+        let scalars = create_random_extension_field_element_scalars(1);
+
+        group.bench_with_input(
+            BenchmarkId::from_parameter(format!("Rows: 2ˆ{}", log_2_rows)),
+            &polys,
+            |b, polys| {
+                b.iter(|| {
+                    fold_multilinear_in_large_field(
+                        black_box(polys),
+                        black_box(&scalars),
+                        RoundType::WithNoSkips,
+                    )
+                })
+            },
+        );
+    }
+
+    group.finish();
+}
+
+// Represents the case: Rounds > 0 (polynomials in extension field), skips == 1, scalars in extension field.
+fn bench_fold_multilinear_in_large_field_with_extension_field_polys(c: &mut Criterion) {
+    let mut group = c.benchmark_group(
+        "fold_multilinear_in_large_field with extension field polys (Rounds > 0) and skips = 1",
+    );
+    group
+        .sample_size(SAMPLE_SIZE)
+        .measurement_time(Duration::from_secs(MEASUREMENT_TIME));
+
+    let log2_rows_list = [22];
+
+    for &log_2_rows in &log2_rows_list {
+        let polys = create_random_extension_field_element_polys(log_2_rows);
+        let scalars = create_random_extension_field_element_scalars(1);
+
+        group.bench_with_input(
+            BenchmarkId::from_parameter(format!("Rows: 2ˆ{}", log_2_rows)),
+            &polys,
+            |b, polys| {
+                b.iter(|| {
+                    fold_multilinear_in_large_field(
+                        black_box(polys),
+                        black_box(&scalars),
+                        RoundType::WithNoSkips,
+                    )
+                })
+            },
+        );
+    }
+
+    group.finish();
+}
+
+// Represents the case: Round 0 (polynomials in base field), skips == 4, scalars in extension field.
 fn bench_fold_multilinear_in_large_field_with_skip(c: &mut Criterion) {
-    let mut group = c.benchmark_group("fold_multilinear_large_field_with_skip");
+    let mut group = c.benchmark_group(
+        "fold_multilinear_in_large_field with base field polys (Round 0) and skips = 4",
+    );
     group
         .sample_size(SAMPLE_SIZE)
         .measurement_time(Duration::from_secs(MEASUREMENT_TIME));
@@ -120,270 +256,12 @@ fn bench_fold_multilinear_in_large_field_with_skip(c: &mut Criterion) {
     group.finish();
 }
 
-// Subsequent rounds (skip == 1 and polys in extension field), scalars in base field (small)
-fn bench_fold_multilinear_in_small_field_with_extension_field_polys(c: &mut Criterion) {
-    let mut group = c.benchmark_group("fold_multilinear_small_field_with_extension_field_polys");
-    group
-        .sample_size(SAMPLE_SIZE)
-        .measurement_time(Duration::from_secs(MEASUREMENT_TIME));
-
-    let log2_rows_list = [22];
-
-    for &log_2_rows in &log2_rows_list {
-        let polys = create_random_extension_field_element_polys(log_2_rows);
-        let scalars = create_random_base_field_element_scalars(1);
-
-        group.bench_with_input(
-            BenchmarkId::from_parameter(format!("Rows: 2ˆ{}", log_2_rows)),
-            &polys,
-            |b, polys| {
-                b.iter(|| {
-                    fold_multilinear_in_small_field(
-                        black_box(polys),
-                        black_box(&scalars),
-                        RoundType::WithNoSkips,
-                    )
-                })
-            },
-        );
-    }
-
-    group.finish();
-}
-
-// Subsequent rounds (skip == 1 and polys in extension field), scalars in extension field (large)
-fn bench_fold_multilinear_in_large_field_with_extension_field_polys(c: &mut Criterion) {
-    let mut group = c.benchmark_group("fold_multilinear_large_field_with_extension_field_scalars");
-    group
-        .sample_size(SAMPLE_SIZE)
-        .measurement_time(Duration::from_secs(MEASUREMENT_TIME));
-
-    let log2_rows_list = [22];
-
-    for &log_2_rows in &log2_rows_list {
-        let polys = create_random_extension_field_element_polys(log_2_rows);
-        let scalars = create_random_extension_field_element_scalars(1);
-
-        group.bench_with_input(
-            BenchmarkId::from_parameter(format!("Rows: 2ˆ{}", log_2_rows)),
-            &polys,
-            |b, polys| {
-                b.iter(|| {
-                    fold_multilinear_in_large_field(
-                        black_box(polys),
-                        black_box(&scalars),
-                        RoundType::WithNoSkips,
-                    )
-                })
-            },
-        );
-    }
-
-    group.finish();
-}
-
-/////
-
-// // Base field polynomials -> first round.
-// // first round -> skip == 4
-// // When is this benchmark useful?
-// fn bench_fold_multilinear_in_small_field_with_base_field_polys(c: &mut Criterion) {
-//     let mut group = c.benchmark_group("fold_multilinear_small_field_with_base_field_polys");
-//     group
-//         .sample_size(SAMPLE_SIZE)
-//         .measurement_time(Duration::from_secs(MEASUREMENT_TIME));
-
-//     let log2_rows_list = [22];
-
-//     for &log_2_rows in &log2_rows_list {
-//         let polys = create_random_base_field_element_polys(log_2_rows);
-//         let scalars = create_random_base_field_element_scalars(1);
-
-//         group.bench_with_input(
-//             BenchmarkId::from_parameter(format!("Rows: 2ˆ{}", log_2_rows)),
-//             &polys,
-//             |b, polys| {
-//                 b.iter(|| {
-//                     fold_multilinear_in_small_field(
-//                         black_box(polys),
-//                         black_box(&scalars),
-//                         RoundType::WithNoSkips,
-//                     )
-//                 })
-//             },
-//         );
-//     }
-
-//     group.finish();
-// }
-
-// // Extension field polynomials -> Subsequent rounds.
-// // Subsequent rounds -> skip == 1
-// // This benchmark represent every round > 0.
-// fn bench_fold_multilinear_in_small_field_with_extension_field_polys(c: &mut Criterion) {
-//     let mut group = c.benchmark_group("fold_multilinear_small_field_with_extension_field_polys");
-//     group
-//         .sample_size(SAMPLE_SIZE)
-//         .measurement_time(Duration::from_secs(MEASUREMENT_TIME));
-
-//     let log2_rows_list = [22];
-
-//     for &log_2_rows in &log2_rows_list {
-//         let polys = create_random_extension_field_element_polys(log_2_rows);
-//         let scalars = create_random_base_field_element_scalars(1);
-
-//         group.bench_with_input(
-//             BenchmarkId::from_parameter(format!("Rows: 2ˆ{}", log_2_rows)),
-//             &polys,
-//             |b, polys| {
-//                 b.iter(|| {
-//                     fold_multilinear_in_small_field(
-//                         black_box(polys),
-//                         black_box(&scalars),
-//                         RoundType::WithNoSkips,
-//                     )
-//                 })
-//             },
-//         );
-//     }
-
-//     group.finish();
-// }
-
-// // This benchmark represent the first round: skips == 4 and the polynimoials are in the base field.
-// fn bench_fold_multilinear_in_small_field_with_skip(c: &mut Criterion) {
-//     let mut group = c.benchmark_group("fold_multilinear_small_field_with_skip");
-//     group
-//         .sample_size(SAMPLE_SIZE)
-//         .measurement_time(Duration::from_secs(MEASUREMENT_TIME));
-
-//     let log2_rows_list = [22];
-//     let log_2_scalars = 4; // Skip = 4
-
-//     for &log_2_rows in &log2_rows_list {
-//         let polys = create_random_base_field_element_polys(log_2_rows);
-//         let scalars = create_random_base_field_element_scalars(log_2_scalars);
-
-//         group.bench_with_input(
-//             BenchmarkId::from_parameter(format!("Rows: 2ˆ{}", log_2_rows)),
-//             &polys,
-//             |b, polys| {
-//                 b.iter(|| {
-//                     fold_multilinear_in_small_field(
-//                         black_box(polys),
-//                         black_box(&scalars),
-//                         RoundType::WithSkips,
-//                     )
-//                 })
-//             },
-//         );
-//     }
-
-//     group.finish();
-// }
-
-// // This benchmark is for the first round, where the polynomials are in the base field.
-// // First round -> skips == 4
-// // When is this benchmark useful?
-// fn bench_fold_multilinear_in_large_field_with_base_field_polys(c: &mut Criterion) {
-//     let mut group = c.benchmark_group("fold_multilinear_large_field_with_base_field_scalars");
-//     group
-//         .sample_size(SAMPLE_SIZE)
-//         .measurement_time(Duration::from_secs(MEASUREMENT_TIME));
-
-//     let log2_rows_list = [22];
-
-//     for &log_2_rows in &log2_rows_list {
-//         let polys = create_random_base_field_element_polys(log_2_rows);
-//         let scalars = create_random_extension_field_element_scalars(1);
-
-//         group.bench_with_input(
-//             BenchmarkId::from_parameter(format!("Rows: 2ˆ{}", log_2_rows)),
-//             &polys,
-//             |b, polys| {
-//                 b.iter(|| {
-//                     fold_multilinear_in_large_field(
-//                         black_box(polys),
-//                         black_box(&scalars),
-//                         RoundType::WithNoSkips,
-//                     )
-//                 })
-//             },
-//         );
-//     }
-
-//     group.finish();
-// }
-
-// // This benchmark is for the subsequent rounds, where the polynomials are in the extension field and skips == 1.
-// fn bench_fold_multilinear_in_large_field_with_extension_field_polys(c: &mut Criterion) {
-//     let mut group = c.benchmark_group("fold_multilinear_large_field_with_extension_field_scalars");
-//     group
-//         .sample_size(SAMPLE_SIZE)
-//         .measurement_time(Duration::from_secs(MEASUREMENT_TIME));
-
-//     let log2_rows_list = [22];
-
-//     for &log_2_rows in &log2_rows_list {
-//         let polys = create_random_extension_field_element_polys(log_2_rows);
-//         let scalars = create_random_extension_field_element_scalars(1);
-
-//         group.bench_with_input(
-//             BenchmarkId::from_parameter(format!("Rows: 2ˆ{}", log_2_rows)),
-//             &polys,
-//             |b, polys| {
-//                 b.iter(|| {
-//                     fold_multilinear_in_large_field(
-//                         black_box(polys),
-//                         black_box(&scalars),
-//                         RoundType::WithNoSkips,
-//                     )
-//                 })
-//             },
-//         );
-//     }
-
-//     group.finish();
-// }
-
-// // Skip == 4, round = 0, polynomials in the base field.
-// fn bench_fold_multilinear_in_large_field_with_skip(c: &mut Criterion) {
-//     let mut group = c.benchmark_group("fold_multilinear_large_field_with_skip");
-//     group
-//         .sample_size(SAMPLE_SIZE)
-//         .measurement_time(Duration::from_secs(MEASUREMENT_TIME));
-
-//     let log2_rows_list = [22];
-//     let log_2_scalars = 4; // Skip = 4
-
-//     for &log_2_rows in &log2_rows_list {
-//         let polys = create_random_base_field_element_polys(log_2_rows);
-//         let scalars = create_random_extension_field_element_scalars(log_2_scalars);
-
-//         group.bench_with_input(
-//             BenchmarkId::from_parameter(format!("Rows: 2ˆ{}", log_2_rows)),
-//             &polys,
-//             |b, polys| {
-//                 b.iter(|| {
-//                     fold_multilinear_in_large_field(
-//                         black_box(polys),
-//                         black_box(&scalars),
-//                         RoundType::WithSkips,
-//                     )
-//                 })
-//             },
-//         );
-//     }
-
-//     group.finish();
-// }
-
 criterion_group!(
     benches,
-    //bench_fold_multilinear_in_small_field_with_base_field_polys,
+    bench_fold_multilinear_in_small_field_with_base_field_polys,
     bench_fold_multilinear_in_small_field_with_extension_field_polys,
     bench_fold_multilinear_in_small_field_with_skip,
-    //bench_fold_multilinear_in_large_field_with_base_field_polys,
+    bench_fold_multilinear_in_large_field_with_base_field_polys,
     bench_fold_multilinear_in_large_field_with_extension_field_polys,
     bench_fold_multilinear_in_large_field_with_skip
 );
